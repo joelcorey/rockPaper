295ad14ab503dad44762728b8c4cfb114bd757c9
Joel Corey <joelcorey@fastmail.com>
2018-03-23 07:49:12 -0600

    Init

---
+++ rockPaperGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockPaperGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // commit consistently
+            // properly incorporate inheritance
+            // 
+
+            GameConfig gameConfig = new GameConfig();
+            
+            foreach (var gesture in gameConfig.gestures)
+            {
+                Console.WriteLine(gesture);
+            }
+        }
+    }
+}
