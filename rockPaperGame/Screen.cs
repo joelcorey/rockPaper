39de9f3c53d547656e666e9ba34b137dc84e43b3
joel <joeloregonmines@gmail.com>
2018-03-25 17:32:42 -0600

    Add Screen class with initial methods for changing displayed information

---
+++ rockPaperGame/Screen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rockPaperGame
+{
+    class Screen
+    {
+        public void clearScreen()
+        {
+            Console.Clear();
+        }
+
+        public void mainMenu(List<string> gestures)
+        {
+            clearScreen();
+            Console.WriteLine("Welcome to {0}, {1}, {2}, {3}, {4}", gestures[0], gestures[1], gestures[2], gestures[3], gestures[4]);
+            Console.WriteLine("");
+            Console.WriteLine("Verse (P)layer or (C)omputer?");
+        }
+
+        public void gameInProgress(int whatRound)
+        {
+            clearScreen();
+            Console.WriteLine("Round: {0}", Convert.ToString(whatRound));
+        }
+    }
+}
